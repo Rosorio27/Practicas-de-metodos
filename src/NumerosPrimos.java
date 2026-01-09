94a122964c845d26d2a84782eef5e554b44d70df
Ramon Osoiro <ramon.osorio1989@gmail.com>
2026-01-08 21:43:51 -0600

    Programa completado con exito

--- src/NumerosPrimos.java
+++ src/NumerosPrimos.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class NumerosPrimos {
+    public static void main(String[] args) {
+        Scanner lr = new Scanner(System.in);
+        System.out.print("Introduzca un numero positivo: ");
+        int n = lr.nextInt();
+
+        boolean esPrimo = true;
+        for (int i=2; i<n; i++) {
+            if ((n % i) == 0) {
+                esPrimo = false;
+            }
+        }
+
+        if (esPrimo) {
+            System.out.println("El " + n + " es primo.");
+        } else {
+            System.out.println("El " + n + " no es primo.");
+        }
+    }
 }
